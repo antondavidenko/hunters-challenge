1f713cb67f96da4fe70faf7df3b4838828e4b25d
antondavidenko <anton.davidenko@gmail.com>
2019-07-26 00:17:48 +0300

    v0.8 - [npm] add npm lib package builder; remove js files from bin folder;

---
+++ libs.js
@@ -0,0 +1,20 @@
+// 
+// npm dependencies library
+//
+(function(scope) {
+	'use-strict';
+	scope.__registry__ = Object.assign({}, scope.__registry__, {
+		// list npm modules required in Haxe
+		'react': require('react'),
+		'react-dom': require('react-dom'),
+		'redux': require('redux'),
+		'phaser': require('phaser'),
+		'howler': require('howler')
+	});
+	
+	if (process.env.NODE_ENV !== 'production') {
+		// enable hot-reload
+		require('haxe-modular');
+	}
+	
+})(typeof $hx_scope != "undefined" ? $hx_scope : $hx_scope = {});
